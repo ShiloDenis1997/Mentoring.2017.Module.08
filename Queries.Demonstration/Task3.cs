c2ae25e3edb52ac6b8e24e5d924c5d572e2619f9
Dzianis_Shyla <Dzianis_Shyla@epam.com>
2017-12-04 17:49:23 +0300

    query to change product to same for not shipped orders added

--- Queries.Demonstration/Task3.cs
+++ Queries.Demonstration/Task3.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LinqToDB;
 using LinqToDB.Data;
 using Northwind.Model.LinqToDb;
@@ -32,13 +29,13 @@ namespace Queries.Demonstration
         [Test]
         public void Add_new_Employee_with_Territories()
         {
-            Employee newEmployee = new Employee { FirstName = "Dzianis", LastName = "Shyla"};
+            Employee newEmployee = new Employee { FirstName = "Dzianis", LastName = "Shyla" };
             try
             {
                 _connection.BeginTransaction();
                 newEmployee.EmployeeId = Convert.ToInt32(_connection.InsertWithIdentity(newEmployee));
                 _connection.Territories.Where(t => t.TerritoryDescription.Length <= 5)
-                    .Insert(_connection.EmployeeTerritories, t => new EmployeeTerritory {EmployeeId = newEmployee.EmployeeId, TerritoryId = t.TerritoryId});
+                    .Insert(_connection.EmployeeTerritories, t => new EmployeeTerritory { EmployeeId = newEmployee.EmployeeId, TerritoryId = t.TerritoryId });
                 _connection.CommitTransaction();
             }
             catch
@@ -107,16 +104,33 @@ namespace Queries.Demonstration
         }
 
         [Test]
-        public void Replace_Product_with_the_same_in_NotShipped_Orders()
+        public void Replace_Product_with_the_same_in_NotShipped_Orders_plenty_of_queries()
         {
-            var updatedRows = _connection.OrderDetails.LoadWith(od => od.Order)
-                .Where(od => od.Order.ShippedDate == null)
-                .Update(od => new OrderDetail
-                {
-                    ProductId = _connection.Products.First(p => !_connection.OrderDetails.Where(t => t.OrderId == od.OrderId)
-                            .Any(t => t.ProductId == p.ProductId)).ProductId
-                });
-            Console.WriteLine(updatedRows);
+            var orderDetails = _connection.OrderDetails.LoadWith(od => od.Order)
+                .Where(od => od.Order.ShippedDate == null).ToList();
+            foreach (var orderDetail in orderDetails)
+            {
+                _connection.OrderDetails.LoadWith(od => od.Product).Update(od => od.OrderId == orderDetail.OrderId && od.ProductId == orderDetail.ProductId,
+                    od => new OrderDetail
+                    {
+                        ProductId = _connection.Products.First(p => !_connection.OrderDetails.Where(t => t.OrderId == od.OrderId)
+                                .Any(t => t.ProductId == p.ProductId) && p.CategoryId == od.Product.CategoryId).ProductId
+                    });
+            }
+        }
+
+        [Test]
+        public void Replace_Product_with_the_same_in_NotShippedOrders_one_query()
+        {
+            var updatedRows = _connection.OrderDetails.LoadWith(od => od.Order).LoadWith(od => od.Product)
+                .Where(od => od.Order.ShippedDate == null).Update(
+                    od => new OrderDetail
+                    {
+                        ProductId = _connection.Products.First(p => p.CategoryId == od.Product.CategoryId && p.ProductId > od.ProductId) != null
+                            ? _connection.Products.First(p => p.CategoryId == od.Product.CategoryId && p.ProductId > od.ProductId).ProductId
+                            : _connection.Products.First(p => p.CategoryId == od.Product.CategoryId).ProductId
+                    });
+            Console.WriteLine($"{updatedRows} rows updated");
         }
     }
 }
